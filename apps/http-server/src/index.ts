82779b29089c11bf2ca46e1043448eaaa71b5d10
IamAshishSingh02 <psssasds212004@gmail.com>
2026-04-15 14:05:23 +0000

    testing cd pipeline

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.status(200).json({ 
     status: "ok",
-    message: "App is healthy!", 
+    message: "App is fucking healthy!", 
   });
 });
 
